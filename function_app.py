5194ef39c9f9252661e1c2865ec58715b3497642
Brede Fritjof Klausen <brede@fritjof.no>
2024-05-13 22:23:42 +0200

    Add timer function

--- function_app.py
+++ function_app.py
@@ -19,8 +19,8 @@ def start_bot(webhook_url):
 
 
 # Every Monday at 09:00, aka 11:00 in Norwegian time
-@app.schedule(schedule="0 0 9 * * Mon", arg_name="myTimer", run_on_startup=False, use_monitor=True)
-def monday_timer_trigger(myTimer: func.TimerRequest) -> None:
+@app.timer_trigger(schedule="0 0 9 * * Mon", arg_name="myTimer", run_on_startup=False, use_monitor=True) 
+def monday_timer_trigger_discord(myTimer: func.TimerRequest) -> None:
     time_now = datetime.now()
     if myTimer.past_due:
         logging.warning(f'The timer is past due @ {time_now}')
@@ -30,19 +30,3 @@ def monday_timer_trigger(myTimer: func.TimerRequest) -> None:
     url = os.environ["WEBHOOK_URL"]
     start_bot(url)
 
-
-@app.route(route="force_monday_timer_trigger", auth_level=func.AuthLevel.ADMIN)
-def force_monday_timer_trigger(req: func.HttpRequest) -> func.HttpResponse:
-    time_now = datetime.now()
-
-    is_test = req.params.get('test')
-    url = os.environ["WEBHOOK_URL_TEST"]
-    if is_test:
-        logging.info(f'Python HTTP trigger function processed a test-request @ {time_now}')
-    else:
-        logging.info(f'Python HTTP trigger function processed a request @ {time_now}')
-        url = os.environ["WEBHOOK_URL"]
-
-    start_bot(url)
-
-    return func.HttpResponse(f'Python timer trigger function executed @ {time_now}')
