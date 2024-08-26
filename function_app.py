5b400b16911b00992ad2552e5b8996db8a807e6a
Brede Fritjof Klausen <brede.klausen@obos.no>
2024-08-26 11:09:27 +0200

    [discord-bot] Change to 09:30

--- function_app.py
+++ function_app.py
@@ -1,4 +1,3 @@
-import json
 import logging
 import os
 from datetime import datetime
@@ -22,8 +21,8 @@ def start_bot(webhook_url):
     logging.info(f'Python timer trigger function executed. The leaderboard had {len(athletes)} athletes')
 
 
-# Every Monday at 09:00, aka 11:00 in Norwegian time
-@app.timer_trigger(schedule="0 0 9 * * Mon", arg_name="myTimer", run_on_startup=False, use_monitor=True)
+# Every Monday at 09:30, aka 11:30 in Norwegian time
+@app.timer_trigger(schedule="0 30 9 * * Mon", arg_name="myTimer", run_on_startup=False, use_monitor=True)
 def monday_timer_trigger_discord(myTimer: func.TimerRequest) -> None:
     time_now = datetime.now()
     if myTimer.past_due:
